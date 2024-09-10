6a6455d8b3f08c51dc9760217d5389727a7d0078
8 bit <thesolibrary@gmail.com>
2024-09-10 17:09:28 -0400

    jj

---
+++ runic/js/jupiter-init.js
@@ -0,0 +1,5 @@
+window.Jupiter.init({
+    displayMode: "integrated",
+    integratedTargetId: "integrated-terminal",
+    endpoint: "https://mainnet.helius-rpc.com/?api-key=6b52d42b-5d24-4841-a093-02b0d2cc9fc0",
+});
